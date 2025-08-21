0587e516e4cac1e008a33eed88de3de5946830eb
Bhakta Bhusan Das <bhaktabhaktabhakta@gmail.com>
2025-08-21 11:02:03 +0530

    Updated the version to 1.12.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world from dockerized Node.js App Version: 1.11</h1>');
+    res.send('<h1>Hello world from dockerized Node.js App Version: 1.12</h1>');
 
 });
 
